98eff9a6a2510fb89e216b899bcdd54ccec5e012
pmz10 <welcome_to_pmz@hotmail.com>
2024-11-10 21:57:08 -0600

    Terminado el navBAr

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/Components";
 
 export default function GeneralLayout({
   children
@@ -5,9 +6,12 @@ export default function GeneralLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex flex-col items-center p-24">
-      <span>Hola Mundo</span>
-      {children}
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center p-24">
+        <span>Hola Mundo</span>
+        {children}
+      </main>
+    </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <span className="text-5xl">Hola Mundo</span>
+      <Link href="/about">About Page</Link>
     </div>
   );
 }
